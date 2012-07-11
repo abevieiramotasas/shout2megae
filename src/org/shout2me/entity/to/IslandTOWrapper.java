98a3e11439b2ec510bdfc4118051c76c32939588
Abelardo <abbbekgb@gmail.com>
2012-07-11 13:46:06 -0300

    configuracoes

--- src/org/shout2me/entity/to/IslandTOWrapper.java
+++ src/org/shout2me/entity/to/IslandTOWrapper.java
@@ -1,12 +1,18 @@
 package org.shout2me.entity.to;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "islands")
 public class IslandTOWrapper {
+
 	private List<IslandTO> islands;
+
+	public IslandTOWrapper() {
+		this.islands = new ArrayList<IslandTO>();
+	}
 
 	public void setIslands(List<IslandTO> islands) {
 		this.islands = islands;
--- src/org/shout2me/entity/to/JAXBContextResolver.java
+++ src/org/shout2me/entity/to/JAXBContextResolver.java
@@ -1,22 +1,22 @@
 package org.shout2me.entity.to;
 
 import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.api.json.JSONJAXBContext;
 
-// retirando para usar default
-//@Provider
+@Provider
 public class JAXBContextResolver implements ContextResolver<JAXBContext> {
 
 	private JAXBContext context;
 	private Class<?>[] types = { IslandTOWrapper.class };
 
 	public JAXBContextResolver() throws JAXBException {
-		this.context = new JSONJAXBContext(JSONConfiguration.mappedJettison()
-				.build(), types);
+		this.context = new JSONJAXBContext(JSONConfiguration.mapped()
+				.arrays("islands").build(), types);
 	}
 
 	@Override
